2004b6318757d13f3889c7a024675c4786937564
Derek Hammer <hammer@tablexi.com>
2021-03-16 15:08:16 -0600

    Add component to render

--- app/javascript/packs/list.tsx
+++ app/javascript/packs/list.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import ReactDOM from 'react-dom';
 
+type Item = {
+  id: number;
+  name: string;
+}
+
+const ItemComponent = ({ item }: { item: Item }): JSX.Element => {
+  return (<div>{item.name}</div>);
+
+};
+
 const ListApp = (): JSX.Element => {
-  return (<div>Hello World!</div>);
+  const [items, setItems] = useState<Item[]>([]);
+
+  useEffect(() => {
+    fetch('/items', { headers: { 'Accept': 'application/json' } }).then(async (response) => {
+      setItems((await response.json()) as Item[]);
+    });
+  }, [fetch]);
+
+  if (items.length === 0) {
+    return (<div style={{padding: 20}}>There are no items.</div>);
+  }
+
+  return (
+    <div style={{padding: 20}}>
+      {items.map(item => <ItemComponent key={item.id} item={item} />)}
+    </div>
+  )
 };
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <ListApp />,
-    document.body.appendChild(document.createElement('div')),
+    document.getElementById('list-app')
   )
 })
